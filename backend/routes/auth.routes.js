ecf3dbbbe1809fb8d52be5e0be9667efab87c4de
lucianohuwercominotti-prog <luciano.huwer.cominotti@gmail.com>
2026-03-04 19:12:18 -0300

    Update auth.routes.js

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const pool = require('../config/database');
+const { pool } = require('../config/database');
 
 // Cadastro de Admin
 router.post('/admin/register', async (req, res) => {
